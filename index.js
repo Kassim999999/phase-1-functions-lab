8aac583157c543f77f86a5ebb54687ca9899ca39
Kassim <kassimrooney@gmail.com>
2025-03-13 12:15:28 +0300

    functions lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    if (blocks > 42) {
+        return blocks - 42
+    }
+    else{
+        return 42 - blocks;
+    }
+}
+
+function distanceFromHqInFeet(blocks) {
+    return distanceFromHqInBlocks(blocks) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(destination - start) * 264
+}
+
+function calculatesFarePrice(start, destination) {
+    let feet = Math.abs(destination - start) * 264;
+
+    if (feet <= 400) {
+        return 0
+    }
+    else if (feet > 400 && feet <= 2000) {
+        return (feet - 400) * 0.02;
+    }
+    else if (feet > 2000 && feet <= 2500) {
+        return 25
+    }
+    else{
+        return 'cannot travel that far'
+    }
+}
